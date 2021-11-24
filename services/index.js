a454899cd9cf2c04800594ca91e80ad86e212c78
Codefreak69 <iamvikashkumar30@gmail.com>
2021-11-24 18:39:03 +0530

    Added PostDetails function....and logic to fetch article from the API

--- services/index.js
+++ services/index.js
@@ -36,6 +36,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: {slug :$slug}){
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage { 
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              raw
+            }
+          }
+        }
+  `;
+  const result = await request(graphqlAPI, query , {slug });
+
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
    query GetPostDetails() {
@@ -55,7 +89,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories , slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -74,7 +108,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `;
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, {categories , slug});
   return result.posts;
 };
 
