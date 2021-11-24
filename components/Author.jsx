d3db54c7c10cb956cc7f3bbcbc93157b9401a773
Codefreak69 <iamvikashkumar30@gmail.com>
2021-11-24 18:55:30 +0530

    Added Author section to the Blog

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Author = () => {
+import Image from "next/image"
+const Author = ({ author}) => {
     return (
-        <div>
-            <h1>Author</h1>
+        <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-pink-500   ">
+            <div className="absolute left-0 -top-10 right-2">
+            <Image 
+            alt={author.name}
+            unoptimized
+            height="100px"
+            width="100px"
+            className='align-middle rounded-full '
+            src={author.photo.url}
+             />
+             </div>
+             <h3 className="text-white text-xl my-5 font-bold">{author.name}</h3>
+             <p className="text-lg ">{author.bio}</p>
         </div>
     )
 }
